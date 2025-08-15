ec57bf663f873db4ccdd310234530bf599296cc9
Kendas9866cdgit config --global user.name Kendas9866 <kendigabriel29@gmail.com>
2025-08-15 10:41:48 -0300

    Projeto java

---
+++ src/main/java/com/go/go_planner/GoPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.go.go_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoPlannerApplication.class, args);
+	}
+
+}
