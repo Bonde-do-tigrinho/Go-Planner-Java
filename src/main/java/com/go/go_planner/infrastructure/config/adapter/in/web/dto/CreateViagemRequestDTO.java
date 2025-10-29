34ade2657be9703852514fc6a6f73fbc249df7e4
Miguel Lemos Ramos <miguel.ramos3@fatec.sp.gov.br>
2025-10-28 22:48:35 -0300

    Revert "Criar viagem lalala"

--- src/main/java/com/go/go_planner/infrastructure/config/adapter/in/web/dto/CreateViagemRequestDTO.java
+++ src/main/java/com/go/go_planner/infrastructure/config/adapter/in/web/dto/CreateViagemRequestDTO.java
@@ -5,31 +5,20 @@ package com.go.go_planner.infrastructure.config.adapter.in.web.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import java.time.LocalDateTime;
-import java.util.List;
+
+import java.time.LocalDate;
 
 @Data
 public class CreateViagemRequestDTO {
 
-    @NotBlank(message = "O título da viagem é obrigatório.")
-    private String titulo;
+    @NotBlank(message = "O destino é obrigatório.")
+    private String destino;
 
-    @NotBlank(message = "O local de partida é obrigatório.")
-    private String localPartida;
+    @NotNull(message = "A data de início é obrigatória.")
+    private LocalDate dataInicio;
 
-    @NotBlank(message = "O local de destino é obrigatório.")
-    private String localDestino;
+    @NotNull(message = "A data de fim é obrigatória.")
+    private LocalDate dataFim;
 
-    @NotNull(message = "A data e hora de partida são obrigatórias.")
-    private LocalDateTime dataPartida;
-
-    @NotNull(message = "A data e hora de retorno são obrigatórias.")
-    private LocalDateTime dataRetorno;
-
-    @NotBlank(message = "A descrição é obrigatória.")
-    private String descricao;
-
-    @NotBlank(message = "O ID do criador da viagem é obrigatório.")
-    private String criadorViagemId;
-
+    // Outros campos relevantes para a criação da viagem (ex: userId)
 }
